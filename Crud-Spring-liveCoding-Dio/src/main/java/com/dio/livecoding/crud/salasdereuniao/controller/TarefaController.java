6faf10f7f32ee7b8aada5b00a8164699caff9c8a
unknown <jack.matheusch@gmail.com>
2022-03-08 12:35:33 -0300

    Adicionado metodos GetById para Users e Tasks

--- Crud-Spring-liveCoding-Dio/src/main/java/com/dio/livecoding/crud/salasdereuniao/controller/TarefaController.java
+++ Crud-Spring-liveCoding-Dio/src/main/java/com/dio/livecoding/crud/salasdereuniao/controller/TarefaController.java
@@ -3,6 +3,8 @@ package com.dio.livecoding.crud.salasdereuniao.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dio.livecoding.crud.salasdereuniao.exception.ResourceNotFoundException;
 import com.dio.livecoding.crud.salasdereuniao.model.TarefaModel;
 import com.dio.livecoding.crud.salasdereuniao.repository.TarefaRepository;
 
@@ -42,6 +46,19 @@ public class TarefaController {
 	@DeleteMapping(path = "tarefa/deletar/{codigo}")
 	public void deletar(@PathVariable Integer codigo) {
 		repository.deleteById(codigo);
+	}
+	
+	@PutMapping(path = "/tarefa/{codigo}")
+	public ResponseEntity<TarefaModel> atualizarPeloCodigoId(@PathVariable (value = "id") Integer codigo,
+								@Valid @RequestBody TarefaModel tarefaDetails) throws ResourceNotFoundException {
+	TarefaModel tarefa = repository.findById(codigo)
+	 .orElseThrow(() -> new ResourceNotFoundException("Tarefa not found for this id :: " + codigo));
+
+	tarefa.setDescricao(tarefaDetails.getDescricao());
+	tarefa.setPrioridade(tarefaDetails.getPrioridade());
+	final TarefaModel updateTarefa = repository.save(tarefa);
+	return ResponseEntity.ok(updateTarefa);
+
 	}
 
 
--- Crud-Spring-liveCoding-Dio/src/main/java/com/dio/livecoding/crud/salasdereuniao/controller/UsuarioController.java
+++ Crud-Spring-liveCoding-Dio/src/main/java/com/dio/livecoding/crud/salasdereuniao/controller/UsuarioController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dio.livecoding.crud.salasdereuniao.exception.ResourceNotFoundException;
 import com.dio.livecoding.crud.salasdereuniao.model.Room;
 import com.dio.livecoding.crud.salasdereuniao.model.TarefaModel;
 import com.dio.livecoding.crud.salasdereuniao.model.UsuarioModel;
@@ -49,6 +51,19 @@ public class UsuarioController {
 	@DeleteMapping(path = "usuario/deletar/{codigo}")
 	public void deletar(@PathVariable Integer codigo) {
 		repository.deleteById(codigo);
+	}
+	
+	@PutMapping(path = "/usuario/{codigo}")
+	public ResponseEntity<UsuarioModel> atualizarPeloCodigoId(@PathVariable (value = "id") Integer codigo,
+								@Valid @RequestBody UsuarioModel usuarioDetails) throws ResourceNotFoundException {
+	UsuarioModel usuario = repository.findById(codigo)
+	 .orElseThrow(() -> new ResourceNotFoundException("Usuario not found for this id :: " + codigo));
+
+	usuario.setNome(usuarioDetails.getNome());
+	usuario.setEmail(usuarioDetails.getEmail());
+	final UsuarioModel updateUsuario = repository.save(usuario);
+	return ResponseEntity.ok(updateUsuario);
+
 	}
 
     
